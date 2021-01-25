054e6570c0415e8f6a56adc635b395c80cb86357
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-01-25 16:27:41 -0300

    Persistência, validações e exceções
    Variações de buscas

--- osworks-api/osworks-api/src/main/java/com/lduran/osworks/api/controller/ClienteController.java
+++ osworks-api/osworks-api/src/main/java/com/lduran/osworks/api/controller/ClienteController.java
@@ -2,11 +2,9 @@ package com.lduran.osworks.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lduran.osworks.domain.model.Cliente;
@@ -18,12 +16,27 @@ public class ClienteController
 	@Autowired
 	private ClienteRepository clienteRepository;
 
-	@PersistenceContext
-	private EntityManager manager;
-
 	@GetMapping("/clientes")
 	public List<Cliente> listar()
 	{		
 		return this.clienteRepository.findAll();
+	}
+
+	@GetMapping("/clientes/buscando-nome")
+	public List<Cliente> buscarPorNome()
+	{
+		return this.clienteRepository.findByNome("Maria de Gouvea");
+	}
+
+	@GetMapping("/clientes/buscando-nome-parcial")
+	public List<Cliente> buscarPorNomeParcial()
+	{
+		return this.clienteRepository.findByNomeContaining("João");
+	}
+
+	@GetMapping("/clientes/{clienteId}")
+	public Cliente buscar(@PathVariable Long clienteId)
+	{
+		return this.clienteRepository.findById(clienteId).orElse(null);
 	}
 }
--- osworks-api/osworks-api/src/main/java/com/lduran/osworks/domain/repository/ClienteRepository.java
+++ osworks-api/osworks-api/src/main/java/com/lduran/osworks/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.lduran.osworks.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.lduran.osworks.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>
 {
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
 
 }
