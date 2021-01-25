3620d7971e223cbfc9146a6d3282be064c024490
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-01-25 15:36:20 -0300

    Persistência, validações e exceções
    Criação da persistência dos dados no MySQL usando EntityManager;

--- osworks-api/src/main/java/com/lduran/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/lduran/osworks/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.lduran.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,12 @@ import com.lduran.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController
 {
+	@PersistenceContext
+	private EntityManager manager;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar()
-	{
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João da Silva");
-		cliente1.setEmail("joao.da.silva@algaworks.com.br");
-		cliente1.setTelefone("(12) 99999-1111");
-
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria do Carmo");
-		cliente2.setEmail("maria.do.carmo@algaworks.com.br");
-		cliente2.setTelefone("(12) 99999-2222");
-
-		return Arrays.asList(cliente1, cliente2);
+	{		
+		return this.manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
--- osworks-api/src/main/java/com/lduran/osworks/domain/model/Cliente.java
+++ osworks-api/src/main/java/com/lduran/osworks/domain/model/Cliente.java
@@ -1,12 +1,24 @@
 package com.lduran.osworks.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
 	private Long id;
+
 	private String nome;
 	private String email;
 	private String telefone;
