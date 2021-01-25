069c2f7668f465eb29631f038f9129d9799a404f
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-01-25 17:40:05 -0300

    Persistência, validações e exceções
    Conclusão da criação dos End-Points do CRUD

--- osworks-api/src/main/java/com/lduran/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/lduran/osworks/api/controller/ClienteController.java
@@ -4,9 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lduran.osworks.domain.model.Cliente;
@@ -34,5 +40,38 @@ public class ClienteController
 		}
 
 		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente)
+	{		
+		return this.clienteRepository.save(cliente);
+	}
+
+	@PutMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente)
+	{
+		if(!this.clienteRepository.existsById(clienteId))
+		{
+			return ResponseEntity.notFound().build();
+		}
+
+		cliente.setId(clienteId);
+		cliente = this.clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@DeleteMapping("/clientes/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId)
+	{
+		if(!this.clienteRepository.existsById(clienteId))
+		{
+			return ResponseEntity.notFound().build();
+		}
+
+		this.clienteRepository.deleteById(clienteId);
+
+		return ResponseEntity.noContent().build();
 	}
 }
