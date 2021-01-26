3f2e09f53691f09a415fd751d2190891417c4750
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-01-26 13:18:18 -0300

    Persistência, validações e exceções
    Alteração na classe Problema() para apenas retornar campos não nulos;

--- osworks-api/src/main/java/com/lduran/osworks/api/exceptionhandler/Problema.java
+++ osworks-api/src/main/java/com/lduran/osworks/api/exceptionhandler/Problema.java
@@ -3,10 +3,14 @@ package com.lduran.osworks.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@JsonInclude(Include.NON_NULL)
 public class Problema
 {
 	private int status;
